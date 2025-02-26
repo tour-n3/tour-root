26ba2bf2771742c80aa494bba4a5e7f5cfa8cd6a
7zism3 <7zism3@gmail.com>
2025-02-26 19:08:20 +0700

    init - git

--- init.sh
+++ init.sh
@@ -5,5 +5,6 @@ git clone https://github.com/tour-n3/tour-backend.git
 git clone https://github.com/tour-n3/tour-admin.git
 git clone https://github.com/tour-n3/tour-client.git
 git clone https://github.com/tour-n3/tour-other.git
+git clone https://github.com/tour-n3/minimal-api-dev.git
 
 echo "All repositories have been cloned."
