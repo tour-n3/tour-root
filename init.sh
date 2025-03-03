ca48b363bf1b1d499b4c8a97cf02b282781b1015
7zism3 <7zism3@gmail.com>
2025-03-03 17:12:52 +0700

    init - ssh

--- init.sh
+++ init.sh
@@ -7,4 +7,15 @@ git clone https://github.com/tour-n3/tour-client.git
 git clone https://github.com/tour-n3/tour-other.git
 git clone https://github.com/tour-n3/minimal-api-dev.git
 
+
+git clone git@github.com:tour-n3/tour-backend.git
+git clone git@github.com:tour-n3/tour-admin.git
+git clone git@github.com:tour-n3/tour-client.git
+git clone git@github.com:tour-n3/tour-other.git
+git clone git@github.com:tour-n3/minimal-api-dev.git
+
+
+git clone git@github.com:tour-n3/tour-client_demo.git
+git clone git@github.com:tour-n3/tour-admin_demo.git
+
 echo "All repositories have been cloned."
