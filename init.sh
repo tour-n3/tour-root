d75636c7f511080ab412b9a335865b7f4b6c66b7
7zism3 <7zism3@gmail.com>
2025-05-05 18:41:10 +0700

    init - add project

--- init.sh
+++ init.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
 # Clone repositories
-git clone https://github.com/tour-n3/tour-backend.git
-git clone https://github.com/tour-n3/tour-admin.git
-git clone https://github.com/tour-n3/tour-client.git
-git clone https://github.com/tour-n3/tour-other.git
-git clone https://github.com/tour-n3/minimal-api-dev.git
-
+git clone git@github.com:tour-n3/tour-dev-tools.git
+git clone git@github.com:tour-n3/tour-flyway.git
 
 git clone git@github.com:tour-n3/tour-backend.git
 git clone git@github.com:tour-n3/tour-admin.git
